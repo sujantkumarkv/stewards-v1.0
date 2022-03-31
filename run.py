a1397af6c9f54de524e419cf6fd3602f44146387
sujantkumarkv <kumarskv.201cv253@nitk.edu.in>
2022-03-31 23:35:03 +0530

    update procfile and run.py file to check deployment

--- run.py
+++ run.py
@@ -1,4 +1,19 @@
 from app import app
-from app import routes
+from app.preprocess import preprocess
+from flask import render_template
+
+stewards_data = preprocess()
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    #proposals_data = get_proposals()
+    #proposal_number = len(proposals_data)
+    #print(number_prop.number)
+    #if proposal_number != number_prop.number:
+    #    initial_list, number_prop = preprocess()
+    
+
+    return render_template("index.html", stewards_data=stewards_data)
+
 
 app.run(debug=True)
