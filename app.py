34f2c2fc927c2826f7fffecfa456f880ad0f2565
sujantkumarkv <kumarskv.201cv253@nitk.edu.in>
2022-05-14 12:38:35 +0530

    update data, app.py

--- app.py
+++ app.py
@@ -17,6 +17,6 @@ def index():
 """On removing below code, it doesn't run locally and removing it works for deployment. 
     We can infer from this that file's __name__ property, uses and how it's used in local development.
     But things change in deploying. `app.run` isn't needed. Infact, deploy didn't work Ig due to this."""
-#if __name__ == "__main__":
-#    app.run(debug=True)
-app.run()
+if __name__ == "__main__":
+    app.run()
+#app.run()
